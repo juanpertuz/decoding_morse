f1d99f880727b363db28c7b98bb42949c5f09331
Juan Guillermo <125285682+juanpertuz@users.noreply.github.com>
2023-02-23 17:58:23 -0500

    Diccionarios

--- codigo_morse.py
+++ codigo_morse.py
@@ -1,33 +1,31 @@
 def codificar(mensaje):
-    msj = mensaje.upper()
-    msj_morse = []
+    msj_morse = ''
     dict_morse = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
                   'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
                   'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
                   'Y': '-.--', 'Z': '--..'}
-    for letra in msj:
-        if letra in dict_morse:
-            msj_morse.append(dict_morse[letra])
+    for letra in mensaje:
+        if letra.upper() in dict_morse:
+            msj_morse += (dict_morse[letra.upper()]) + ';'
         else:
-            msj_morse.append('  ')
-    print(';'.join(msj_morse))
+            msj_morse += ' ;'
+    print(msj_morse)
 
 
 def decodificar(mensaje_morse):
-    msj_morse = list(mensaje_morse.split(';'))
-    mensaje = []
+    msj_morse = mensaje_morse.split(';')
+    mensaje = ''
     dict_morse_r = {'.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E', '..-.': 'F', '--.': 'G', '....': 'H',
                     '..': 'I', '.---': 'J', '-.-': 'K', '.-..': 'L', '--': 'M', '-.': 'N', '---': 'O', '.--.': 'P',
                     '--.-': 'Q', '.-.': 'R', '...': 'S', '-': 'T', '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X',
                     '-.--': 'Y', '--..': 'Z'}
     for simbol in msj_morse:
         if simbol in dict_morse_r:
-            mensaje.append(dict_morse_r[simbol])
+            mensaje += dict_morse_r[simbol]
         else:
-            mensaje.append('')
-    msj_final = str(''.join(mensaje))
-    print(msj_final)
+            mensaje += ' '
+    print(mensaje)
 
 
-codificar('telefono')
-decodificar('-;.;.-..;.;..-.;---;-.;---')
+codificar('el edificio esta en llamas')
+decodificar('.;.-..; ;.;-..;..;..-.;..;-.-.;..;---; ;.;...;-;.-; ;.;-.; ;.-..;.-..;.-;--;.-;...;')
